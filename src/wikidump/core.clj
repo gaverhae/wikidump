587c85b81083e1f6861a82b5f21da392e0b33758
Gary Verhaegen <gary.verhaegen@gmail.com>
2016-01-24 20:38:06 +0100

    Add stub of code to download wikipedia abstracts

--- src/wikidump/core.clj
+++ src/wikidump/core.clj
@@ -1,7 +1,20 @@
 (ns wikidump.core
+  (:require [org.httpkit.client :as client])
   (:gen-class))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
   (println "Hello, World!"))
+
+
+(comment
+
+  (def files-dir "resources/public/files")
+  (.mkdirs (java.io.File. files-dir))
+  (def url "http://dumps.wikimedia.org/enwiki/latest/enwiki-latest-abstract23.xml")
+  (def file-path (str files-dir "/excerpt23.xml"))
+
+  (client/get url {} (fn [{:keys [body]}] (spit file-path body)))
+
+  )
