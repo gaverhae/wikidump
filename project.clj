428b189513ecb979652fe9ca75ff8c356b2c8f11
Gary Verhaegen <gary.verhaegen@gmail.com>
2016-01-24 19:55:15 +0100

    Add Compojure and http-kit
    Compojure is the de facto standard routing library in Clojure, and I am
    already familiar with it. There may be better options out there for a
    more complex setup (such as sharing routes with ClojureScript or defining
    true REST APIs), but that is not the focus at the moment. I want to get
    something working quickly.
    
    http-kit is a Clojure library for both HTTP client and server. I am not too
    familiar with it but do have some experience; I think it will work fine.
    Plus, it has few dependencies.

--- project.clj
+++ project.clj
@@ -3,7 +3,9 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [compojure "1.4.0"]
+                 [http-kit "2.1.18"]]
   :main ^:skip-aot wikidump.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
